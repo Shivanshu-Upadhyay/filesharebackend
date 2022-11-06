d49c4552af7bb449c6997fc2550c6db9bc1faeea
shivanshu <shivanshu6525@gmail.com>
2022-11-06 13:39:50 +0530

    new

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const dbConnect = require('./config/db')
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 const app = express()
 app.use(express.json())
 app.use(cors({origin:["https://fileshareonline.netlify.app","http://localhost:3000"]}))
